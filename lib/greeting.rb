a311810ab1ac44248bbc1cd87fbf69bf8ec6d4c3
Tiffani Best <tiffanimelissabest@gmail.com>
2020-07-18 22:40:42 +0000

    Done.

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,1 +1,9 @@
-# code the #greeting method here!
+#!/usr/bin/env ruby
+require_relative "../lib/greeting.rb"	require_relative "../lib/greeting.rb"
+
+
+# code your CLI here! 	# code your CLI here!
+puts "Hi! I'm HAL, what's your name?"
+name = gets.strip
+greeting(name) 
+ 6  lib/greeting.rb 
