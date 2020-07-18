f0347203002cf0224d1ac136659798cf1fd5e47c
Tiffani Best <tiffanimelissabest@gmail.com>
2020-07-18 22:40:23 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,5 +1,9 @@
+#!/usr/bin/env ruby
+require_relative "../lib/greeting.rb"	require_relative "../lib/greeting.rb"
 
 
-def greeting(name)
-  puts "Hello #{name}. It's nice to meet you."
-end
+# code your CLI here! 	# code your CLI here!
+puts "Hi! I'm HAL, what's your name?"
+name = gets.strip
+greeting(name) 
+ 6  lib/greeting.rb 
